e9bf9f17e5a358909cb715deea5fa2f5eab00719
Ed Wright <ew_wrightiii@live.com>
2017-12-09 07:20:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return "It's ${days} days until ${holiday}"
+  return "It's 20 days until Mother's Day!"
 }
